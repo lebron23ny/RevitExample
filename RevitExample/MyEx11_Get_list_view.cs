eb230c165707b93f7d57c0cec1b8ac4f18bea9d7
Алексей Согрин <lebron23ny@yandex.ru>
2025-01-30 21:19:22 +0300

    _

---
+++ RevitExample/MyEx11_Get_list_view.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitExample
+{
+    [Transaction(TransactionMode.Manual)]
+    public class MyEx11_Get_list_view : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uIDocument = commandData.Application.ActiveUIDocument;
+            Document document = uIDocument.Document;
+
+            List<View> list = new FilteredElementCollector(document).OfClass(typeof(View)).Cast<View>().ToList();
+            foreach (View view in list)
+            {
+                string name = view.Name;
+                string titel = view.Title;
+                ElementId id = view.Id;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
